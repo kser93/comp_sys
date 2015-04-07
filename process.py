477f06f3ffd7e35fd17edaf36fcca3ba0f89a3d9
Сергей <kserx05@rambler.ru>
2015-04-08 02:20:04 +0300

    изменение формата вывода результата

--- process.py
+++ process.py
@@ -10,5 +10,5 @@ result = connections_between_threads(data['edges'], threads)
 pprint(threads)
 for line in result:
     for el in line:
-        print('{} '.format(0 if el is 0 else el if el is not None else '-'), end='')
+        print('{} '.format(el if el is not None else '-'), end='')
     print()
